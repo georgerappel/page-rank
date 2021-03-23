2bc595457aa2f80d4140cf8bfe18c2dea2c2d93c
George Rappel <george.concei@hotmail.com>
2021-03-23 13:16:58 -0300

    Update numeric_logic_test.py

--- numeric_logic_test.py
+++ numeric_logic_test.py
@@ -5,21 +5,21 @@
 import numpy as np
 
 maxIterations = 10000
-nPages = 4
 
+# Algo source: http://pi.math.cornell.edu/~mec/Winter2009/RalucaRemus/Lecture3/lecture3.html
 # p = 0.05 >> 0.380 / 0.133 / 0.289 / 0.196
 # p = 0.15 >> 0.368 / 0.141 / 0.288 / 0.202 (ideal acording to the author)
 # p = 0.50 >> 0.320 / 0.178 / 0.278 / 0.223
 p = 0.05
 
 v = np.matrix([[1 / 4], [1 / 4], [1 / 4], [1 / 4]])
-
 A = np.matrix([[0, 0, 1, 1 / 2],
                [1 / 3, 0, 0, 0],
                [1 / 3, 1 / 2, 0, 1 / 2],
                [1 / 3, 1 / 2, 0, 0]])
 
-B = (1 / nPages) * np.ones([4, 4])
+nPages = len(v)
+B = (1 / nPages) * np.ones([nPages, nPages])
 
 pageRankM = (1 - p) * A + p * B
 
